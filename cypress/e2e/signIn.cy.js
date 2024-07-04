daba40d82b8d21069aa4a2ae51dc873a320419ad
Artem Musii <musiy.artem@gmail.com>
2024-07-04 21:36:12 +0200

    added login test

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,14 @@
 /// <reference types="cypress" />
 
+const { faker } = require('@faker-js/faker');
+
 describe('Sign In page', () => {
   it('should provide an ability to log in', () => {
+    cy.visit('/login');
 
+    cy.get('[placeholder="Email"]').type(faker.internet.email());
+    cy.get('[placeholder="Password"]').type(faker.internet.password(30));
+
+    cy.get('button').contains('Sign in').should('exist').click();
   });
 });
